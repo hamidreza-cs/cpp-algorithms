83a464984d6e23d7e5d351db24510c4c4474947c
hamireza.rajabloo <hamidreza.hamidreza.rajablo@gmail.com>
2026-09-16 17:21:50 +0330

    f

---
+++ basic-algorithms/max-of-three/max-of-three.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    int max ;
+
+    if( a > b) max = a;
+    else max = b;
+
+    if (c > max)max = c;
+    
+    cout << max;
+
+    return 0;
+}
